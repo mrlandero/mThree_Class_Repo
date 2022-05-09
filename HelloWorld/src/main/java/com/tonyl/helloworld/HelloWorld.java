3b1833d4bdc5e3b0a9c71a592c2b2877db971e4d
Tony <mr.landero@gmail.com>
2022-05-09 08:33:05 -0500

    Hello World in NetBeans

---
+++ HelloWorld/src/main/java/com/tonyl/helloworld/HelloWorld.java
@@ -0,0 +1,17 @@
+
+package com.tonyl.helloworld;
+
+/**
+ *
+ * @author Tony Landero
+ * email: mr.landero@gmail.com
+ * date: 05/09/2022
+ * purpose: My first Hello World program in an IDE
+ * 
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        String name = "Tony";
+        System.out.println("Hello, " + name + "!");
+    }
+}
