be29f84d04bc82de9a73aa1c7e89f00b5e964c38
Tony <mr.landero@gmail.com>
2022-05-18 08:50:41 -0500

    Add HigherScore class to PracticeProgramming Project on NetBeans

---
+++ PracticeProgramming/src/main/java/com/udemy/projects/HigherScore.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.udemy.projects;
+
+/**
+ *
+ * @author Tony Landero
+ */
+public class HigherScore {
+    public static void main(String[] args) {
+        int[] scores = {randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber(), 
+        randomNumber(), randomNumber(), randomNumber(), randomNumber()};
+        
+        System.out.print("Here are the scores: ");
+        for (int i = 0; i < scores.length; i++) {
+            System.out.print(scores[i] + " ");
+        }
+        
+        int highScore = 0;
+        int seat = 0;
+        for (int i =0; i < scores.length; i++) {
+            if (scores[i] > highScore) {
+                highScore = scores[i];
+                seat = i;
+            }
+        }
+        System.out.println("\n\nThe highest score is: " + highScore + ". Impressive!");
+        System.out.println("It's the gentleman in seat: " + seat + ". Give that man a cookie!");
+    }
+    
+    public static int randomNumber() {
+        double randomDouble = Math.random() * 5000;
+        int randomInt = (int)randomDouble;
+        return randomInt;
+    }
+}
